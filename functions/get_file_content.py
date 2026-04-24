a697916d080fc1708a42b1303025ded62d954b56
JKSept <jksept40@gmail.com>
2026-04-24 01:15:40 -0600

    Updated with file access and read testing

---
+++ functions/get_file_content.py
@@ -0,0 +1,29 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    try:
+        working_dir_abspath = os.path.abspath(working_directory)
+        file_path_abspath = os.path.normpath(os.path.join(working_dir_abspath, file_path))
+
+
+        if os.path.commonpath([working_dir_abspath, file_path_abspath]) != working_dir_abspath:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(file_path_abspath):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+        with open(file_path_abspath, "r") as f:
+            file_content_string = f.read(10000)
+
+            if f.read(1):
+                file_content_string += (f'[...File "{file_path}" truncated at 10000 characters]')
+        return file_content_string
+
+
+    except Exception as e:
+        print(f'Error: {e}')
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -2,36 +2,46 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if api_key is None:
-    raise RuntimeError("api key not found...")
+def main():
 
 
-client = genai.Client(api_key=api_key)
-
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-args = parser.parse_args()
-
-response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("api key not found...")
 
 
-token_usage_info = response.usage_metadata
-if token_usage_info is None:
-    raise RuntimeError("usage metadata return none?")
-prompt_tokens = token_usage_info.prompt_token_count
-candidate_tokens = token_usage_info.candidates_token_count
+    client = genai.Client(api_key=api_key)
 
-print(f"Prompt tokens: {prompt_tokens}")
-print(f"Response tokens: {candidate_tokens}")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    generate_content(client, messages, args)
+
+def generate_content(client, messages, args):
+
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+
+    if not response.usage_metadata:
+        raise RuntimeError("API reponse metadata not working...??")
+
+    if args.verbose:
+        print("User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Response:")
+        print(response.text)
+    else:
+        print("Response:")
+        print(response.text)
 
 
-
-print(response.text)
-
-
-
+if __name__ == "__main__":
+    main()
 
